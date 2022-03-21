f1ad4d664ba3ae523b156d65d6a3d0e104a321c5
Alex Chow <flyingacesnoopy48@gmail.com>
2022-03-21 17:46:33 -0400

    stable state

--- dot_counter.py
+++ dot_counter.py
@@ -1,70 +1,139 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from tkinter import colorchooser
+from PIL import Image, ImageTk
 import pdf_manager as pdf
+import zoom
+import pickle
+
+root = Tk()
+
+
+#TODO: Rebuild into classes
+class counter:
+    def __init__(self):
+            self.color = '#000000'
+            self.curr_color= tk.Label(root, text='#', bg='#000000', fg='#000000')
+            self.colors_count= {}
+            self.dot_list=tk.LabelFrame(root, text="Menu")
+            self.pages = []
+            self.canvas = None
+            self.last_circle = None
+
+this = counter()
 
 def create_frame():
-    global dot_list
-    global curr_color
-    if dot_list:
-        dot_list.destroy()
-    dot_list = tk.LabelFrame(root, text="Menu")
-    dot_list.grid(row=0, column=1)
-    curr_color = tk.Label(dot_list, text='#', bg=color, fg=color)
-    curr_color.grid(row=0, column=0)
-    first_label = tk.Label(dot_list, text="<- Current Color").grid(row=0, column=1)
-    button = Button(dot_list, text = "Select color",
-                   command = choose_color)            
-    button.grid(row=1, column=1)
-    return dot_list
 
-def create_circle(x, y, r, canvasName): #center coordinates, radius
+    if this.dot_list:
+        this.dot_list.destroy()
+    this.dot_list = tk.LabelFrame(root, text="Menu")
+    this.dot_list.grid(row=0, column=1)
+    lButton = Button(this.dot_list, text="<- Prev Page",
+                    command=lambda: chng_pg(False))
+    lButton.grid(row=0, column=0)
+    nButton = Button(this.dot_list, text="Next Page ->",
+                    command=lambda: chng_pg(True))
+    nButton.grid(row=0, column=1)
+    this.curr_color = tk.Label(this.dot_list, text='#', bg=this.color, fg=this.color)
+    this.curr_color.grid(row=1, column=0)
+    first_label = tk.Label(
+        this.dot_list, text="<- Current Color").grid(row=1, column=1)
+    button = Button(this.dot_list, text="Select color",
+                    command=choose_color)
+    button.grid(row=2, column=1)
+    return this.dot_list
+
+
+def create_circle(x, y, r, canvasName):  # center coordinates, radius
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
-    return canvasName.create_oval(x0, y0, x1, y1, fill=color, outline=color)
+    return canvasName.create_oval(x0, y0, x1, y1, fill=this.color, outline=this.color)
+
 
 def choose_color():
-    global color
-    global curr_color
     # variable to store hexadecimal code of color
-    color_code = colorchooser.askcolor(title ="Choose color")
-    curr_color.config(bg=color_code[1], fg=color_code[1])
+    color_code = colorchooser.askcolor(title="Choose color")
+    this.curr_color.config(bg=color_code[1], fg=color_code[1])
     print(color_code[1])
-    color = color_code[1]
+    this.color = color_code[1]
+
 
 def draw_circle(event):
-    if color in colors_count:
-        colors_count[color] += 1
+    if this.color in this.colors_count:
+        this.colors_count[this.color] += 1
     else:
-        colors_count[color] = 1
-    print(colors_count)
-    create_circle(event.x,event.y,10,w)
+        this.colors_count[this.color] = 1
+    print(this.colors_count)
+    create_circle(event.x, event.y, 10, this.canvas)
     generate_list()
 
-def generate_list():
-    dot_list = create_frame()
-    r = 2
-    for item in colors_count:
-        label = tk.Label(dot_list, text='#', bg=item, fg=item).grid(row=r, column=0)
-        first_label = tk.Label(dot_list, text=colors_count[item]).grid(row=r, column=1)
-        r+=1
+def del_circle(event):
+    item = this.canvas.find_closest(event.x, event.y)
+    this.canvas.delete(item)
+    this.canvas.bind("<Button 1>", draw_circle)
 
-root = Tk()
-file = pdf.load_pdf()
-color = '#000000'    
-curr_color = tk.Label(root, text='#', bg=color, fg=color)
-colors_count = {}
-dot_list = tk.LabelFrame(root, text="Menu")
-pdf_img = pdf.convert_pdf_to_tk(file)
-w = pdf_img.width() + 50 #get the width dynamically and add a buffer of 50px
-h = pdf_img.height() + 50 #get the width dynamically and add a buffer of 50px
-#setting up a tkinter canvas
-w = Canvas(root, width=w, height=h)
-w.grid(row=0,column=0)
-w.create_image(20,20, anchor='nw', image=pdf_img)
-create_frame()
-w.bind("<Button 1>",draw_circle)
+def delete():
+    this.canvas.bind("<Button 1>", del_circle)
+
+def generate_list():
+    this.dot_list = create_frame()
+    r = 3
+    for item in this.colors_count:
+        label = tk.Label(this.dot_list, text='#', bg=item,
+                        fg=item).grid(row=r, column=0)
+        first_label = tk.Label(
+            this.dot_list, text=this.colors_count[item]).grid(row=r, column=1)
+        r += 1
+
+def chng_pg(next):
+    idx = this.curr_idx
+    # this.pages[idx] = pickle.dumps(this.canvas)
+    # this.canvas.delete()
+    this.pdf_img, this.curr_idx =p.change_page(this.curr_idx, next)
+    # img = this.pdf_img.getimage()
+    wx = this.pdf_img.width() + 50  # get the width dynamically and add a buffer of 50px
+    hx = this.pdf_img.height() + 50  # get the width dynamically and add a buffer of 50px
+    if this.pages[idx] is not None:
+        this.canvas=this.pages[idx].loads()
+    else:
+        this.canvas = create_canvas(wx, hx)
+
+def create_canvas(wx, hx):
+    # w = zoom.CanvasImage(root, this.pdf_img, draw_circle)
+    w = tk.Canvas(root, width=wx, height=hx)
+    w.bind('<Button 1>',draw_circle)
+    w.grid(row=0, column=0)
+    w.create_image(20, 20, anchor='nw', image=this.pdf_img)
+    create_frame()
+    return w
+
+class MainWindow(ttk.Frame):
+    """ Main window class """
+    def __init__(self, mainframe, wx, hx):
+        """ Initialize the main Frame """
+        ttk.Frame.__init__(self, master=mainframe)
+        self.master.title('Advanced Zoom v3.0')
+        w = str(wx)
+        h = str(hx)
+        dim = w+'x'+h
+        self.master.geometry(dim)  # size of the main window
+        self.master.rowconfigure(0, weight=1)  # make the CanvasImage widget expandable
+        self.master.columnconfigure(0, weight=1)
+        this.canvas= create_canvas(wx, hx)  # create widget
+
+p = pdf.pdf_manager(root)
+file, len = p.load_pdf()
+this.pages = [None] * len
+print(this.pages)
+this.pdf_img, this.curr_idx = p.convert_pdf_to_tk(file)
+wx = this.pdf_img.width() + 50  # get the width dynamically and add a buffer of 50px
+hx = this.pdf_img.height() + 50  # get the width dynamically and add a buffer of 50px
+# setting up a tkinter canvas
+this.canvas = create_canvas(wx, hx)
 root.mainloop()
+# app = MainWindow(root, wx, hx)
+# app.mainloop()
--- pdf_manager.py
+++ pdf_manager.py
@@ -2,19 +2,51 @@ import fitz
 from tkinter import *
 import tkinter as tk
 from tkinter import filedialog
+from PIL import Image, ImageTk
 
-def convert_pdf_to_tk(file):
-    print(file)
-    if file is not None:
-        doc = fitz.open(file)
-        page = doc[0]
+class pdf_manager:
+    def __init__(self, root):
+        self.root = root
+        self.doc = None
+
+    def convert_pdf_to_tk(self, file):
+        if file is not None:
+            self.doc = fitz.open(file)
+            return self.change_page(0, False)
+        else:
+            return tk.PhotoImage()
+
+    def load_pdf(self):
+        filename = filedialog.askopenfilename()
+        self.doc = fitz.open(filename)
+        return filename, len(self.doc)
+
+    def change_page(self, curr_idx, next): #we encode next as a bool, True for next, False for last
+        if self.doc is not None:
+            if next and len(self.doc) > curr_idx+1: #if next and we still have pages in doc,
+                return (self.generate_img(self.doc[curr_idx+1]), curr_idx+1) #return a tuple with next page and the new index
+            elif next and len(self.doc) == curr_idx+1: #if next but we're at end of doc
+                return (self.generate_img(self.doc[curr_idx]), curr_idx) #return existing page and new idx
+            elif not next and curr_idx > 0: #if last and this isn't page one,
+                return (self.generate_img(self.doc[curr_idx-1]), curr_idx-1)#return prev page and new idx
+            elif not next and curr_idx == 0:#if last but page one
+                return (self.generate_img(self.doc[0]),0)#return page one and new idx
+
+    def scale_img(self, img):
+        sw = self.root.winfo_screenwidth()
+        sh = self.root.winfo_screenheight()
+        w = img.width
+        h = img.height
+        while sw<w or sh<h:
+            w = (3*w) // 4
+            h = (3*h) // 4
+            img = img.resize((w, h))
+        return ImageTk.PhotoImage(img)
+
+
+    def generate_img(self, page):
         pix = page.get_pixmap()
-        pix1 = fitz.Pixmap(pix, 0) if pix.alpha else pix  # PPM does not support transparency
-        imgdata = pix1.tobytes("ppm")  # extremely fast!
-        return tk.PhotoImage(data = imgdata)
-    else:
-        return tk.PhotoImage()
-
-def load_pdf():
-    filename = filedialog.askopenfilename()
-    return filename
+        img = Image.frombytes("RGB",(pix.width, pix.height), pix.samples)
+        pil_img = self.scale_img(img)
+        return pil_img
+        
